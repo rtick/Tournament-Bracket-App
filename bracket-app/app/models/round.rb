85f7466ae1a838fcb68051d149f55fa9ee14e153
Ryan Tick <rtick@nd.edu>
2015-12-11 12:06:19 -0500

    Added in validations for our models.

--- bracket-app/app/models/round.rb
+++ bracket-app/app/models/round.rb
@@ -1,4 +1,5 @@
 class Round < ActiveRecord::Base
   belongs_to :tournament
   has_many :matches
+  validates :Name, presence: true
 end
--- bracket-app/app/models/team.rb
+++ bracket-app/app/models/team.rb
@@ -1,4 +1,6 @@
 class Team < ActiveRecord::Base
   belongs_to :match
   belongs_to :tournament
+  validates :Name, presence: true
+  validates :tournament_id, presence: true
 end
