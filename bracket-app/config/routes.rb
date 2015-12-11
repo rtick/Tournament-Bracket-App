14908a93f32345d9d96f89ce73b540b6273f8d55
cray1006 <cray@nd.edu>
2015-12-11 12:16:04 -0500

    can now view teams added to tournament

--- bracket-app/config/routes.rb
+++ bracket-app/config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   resources :rounds
   resources :tournaments
   
+  post "tournaments/:id" => "tournaments#show"
+  
   root 'tournaments#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
