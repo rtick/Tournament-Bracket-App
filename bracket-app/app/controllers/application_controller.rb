a558db309ecf09783989168e80e12229abbd204c
cray1006 <cray@nd.edu>
2015-12-14 14:08:02 -0500

    added new layout

--- bracket-app/app/controllers/application_controller.rb
+++ bracket-app/app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  layout "todo_list"
+  
+  before_action :get_all_tournaments
+  
+  def get_all_tournaments
+    @all_tournaments = Tournament.all
+  end
 end
