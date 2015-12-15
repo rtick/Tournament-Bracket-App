aa0afe72300aa98331e4e77b21bc89dd654afc22
cray1006 <cray@nd.edu>
2015-12-15 14:53:49 -0500

    updating a match automatically sends you to the round page

--- bracket-app/app/controllers/matches_controller.rb
+++ bracket-app/app/controllers/matches_controller.rb
@@ -42,8 +42,8 @@ class MatchesController < ApplicationController
   def update
     respond_to do |format|
       if @match.update(match_params)
-        format.html { redirect_to @match, notice: 'Match was successfully updated.' }
-        format.json { render :show, status: :ok, location: @match }
+        format.html { redirect_to @match.round, notice: 'Match was successfully updated.  Updating round' }
+        format.json { render :show, status: :ok, location: @match.round }
       else
         format.html { render :edit }
         format.json { render json: @match.errors, status: :unprocessable_entity }
